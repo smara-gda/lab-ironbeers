e39ae76bf51993dba87213702734b90cc8b9f23d
Smaragda Kalogeropoulou <smaragda_kal@hotmail.com>
2020-12-19 17:26:23 +0100

    adding iteration 6

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 
 const hbs = require('hbs');
@@ -11,11 +12,10 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
@@ -38,11 +38,24 @@ app.get('/random-beer', (req, res) => {
   punkAPI
     .getRandom()
     .then(beersFromAPI => {
-      res.render('random-beer', { beersFromAPI: beersFromAPI });
+      res.render('random-beer', { beersFromAPI });
       console.log('Getting a response', beersFromAPI);
     })
     .catch(error => {
       console.log('I could not gt a random beer', error);
+    });
+});
+
+app.get('/beers/:id', (req, res) => {
+  const id = req.params.id;
+  punkAPI
+    .getBeer(id)
+    .then(beer => {
+      console.log(beer);
+      res.render('beer', { beer });
+    })
+    .catch(error => {
+      console.log(' Could not open that unique beer page', error);
     });
 });
 
