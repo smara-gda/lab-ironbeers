9f92e65cd3ce115ee62c5099c11cfd4004ecb677
Smaragda Kalogeropoulou <smaragda_kal@hotmail.com>
2020-12-19 14:27:24 +0100

    added all mandatory iterations

--- app.js
+++ app.js
@@ -22,4 +22,27 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromAPI => {
+      res.render('beers', { beersFromAPI });
+      // console.log('This is the list of beers', beersFromAPI);
+    })
+    .catch(error => {
+      console.log('I could not get the beers', error);
+    });
+});
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render('random-beer', { responseFromAPI: responseFromAPI[0] });
+      console.log('Getting a response', responseFromAPI);
+    })
+    .catch(error => {
+      console.log('I could not gt a random beer', error);
+    });
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
